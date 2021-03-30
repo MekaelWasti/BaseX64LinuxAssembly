e1965bb5af225cf10981e448c44501eac1e37a0f
Mekael Wasti <mekael_wasti@hotmail.com>
2021-03-30 19:56:26 -0400

    1

--- main.asm
+++ main.asm
@@ -7,15 +7,19 @@ section .text
 
 main:
   ; printf("Hello, world!\n");
-  mov rdi, message
-  mov rax, 0
+  mov rdi, message1
+  ;mov rax, 0
   push rbx
   call printf
   pop rbx
+
+  ;scanf()
 
   ; exit(0)
   mov rax, 0
   call exit
  
 section .data
-  message db "Hello, world!", 0ah, 0dh, 0
+  message1 db "Enter Your First Name: ", 0ah, 0dh, 0
+  message2 db "Enter Your Current Age: ", 0ah, 0dh, 0
+
