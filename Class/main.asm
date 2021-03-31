666457fc14cf6cfd12ad1f6f4d83f541854e5517
Mekael Wasti <mekael_wasti@hotmail.com>
2021-03-31 01:49:33 -0400

    1

---
+++ Class/main.asm
@@ -0,0 +1,65 @@
+extern printf
+extern scanf
+extern exit
+
+global main
+
+section .text
+
+main:
+  mov rdi, promptFormat
+  mov rsi, prompt_name
+  mov rax, 0
+  push rbx
+  call printf
+  pop rbx
+
+  mov rdi, inputFormat_string
+  mov rsi, name
+  mov rax, 0
+  push rcx
+  call scanf
+  pop rcx
+  
+  mov rdi, promptFormat
+  mov rsi, prompt_age
+  mov rax, 0
+  push rbx
+  call printf
+  pop rbx
+
+  mov rdi, inputFormat_int
+  mov rsi, age
+  mov rax, 0
+  push rcx
+  call scanf
+  pop rcx
+  
+
+  mov rdi, resultFormat
+  mov rsi, name
+  mov rax, 0
+  push rbx
+  call printf
+  pop rbx
+  
+
+
+
+
+  ; exit(0)
+  mov rax, 0
+  call exit
+ 
+section .data
+  prompt_name db "Enter Your First Name: ", 0
+  prompt_age db "Enter Your Current Age: ", 0
+  promptFormat db "%s", 0
+
+  inputFormat_string db "%s", 0
+  inputFormat_int db "%d", 0
+  name dq 0
+  age dq 0
+
+  resultFormat db "%s is %d years old.", 0
+
--- Other/main.asm
+++ Other/main.asm
@@ -1,4 +1,5 @@
 extern printf
+extern scanf
 extern exit
 
 global main
@@ -7,19 +8,21 @@ section .text
 
 main:
   ; printf("Hello, world!\n");
-  mov rdi, message1
+  mov rdi, prompt_name
   ;mov rax, 0
   push rbx
   call printf
   pop rbx
 
-  ;scanf()
+  ;scanf("%d", &age)
+  scanf("%d", &age)
 
   ; exit(0)
   mov rax, 0
   call exit
  
 section .data
-  message1 db "Enter Your First Name: ", 0ah, 0dh, 0
-  message2 db "Enter Your Current Age: ", 0ah, 0dh, 0
+  prompt_name db "Enter Your First Name: ", 0ah, 0dh, 0
+  prompt_age db "Enter Your Current Age: ", 0ah, 0dh, 0
+  age dw 16
 
